e08ab84dfcb975050082bc00b5b6a1ba9d44a69a
ppeums <dlrlqma006@naver.com>
2021-07-22 21:52:09 +0900

    Add: 인터페이스 사용하기

---
+++ src/interfaceimplements/practice2/Meter.java
@@ -0,0 +1,7 @@
+package interfaceimplements.practice2;
+
+public interface Meter {
+//    public int BASE_FARE = 3000; // 기본요금(인터페이스에 정의한 변수는 상수라서 변경할 수 없습니다.)
+    public abstract void start();
+    public abstract int stop(int distance);
+}
---
+++ src/interfaceimplements/practice2/Taxi.java
@@ -0,0 +1,15 @@
+package interfaceimplements.practice2;
+
+public class Taxi implements Meter {
+    public int BASE_FARE;
+
+    public void start() {
+        System.out.println("운행을 시작합니다.");
+    }
+
+    public int stop(int distance) {
+        int fare = BASE_FARE + distance * 2;
+        System.out.println("운행을 종료합니다. 요금은 " + fare + "원 입니다.");
+        return fare;
+    }
+}
