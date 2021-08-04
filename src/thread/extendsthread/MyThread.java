468487e0810f98cc72d4bb6ca731f3be77952683
ppeums <dlrlqma006@naver.com>
2021-08-04 20:10:00 +0900

    Add: 쓰레드 만들기(extends Thread)

---
+++ src/thread/extendsthread/MyThread.java
@@ -0,0 +1,40 @@
+package thread.extendsthread;
+
+/**
+ * 쓰레드를 만드는 방법 1
+ * - Thread 클래스를 상속받아서 쓰레드를 생성하는 방법
+ */
+public class MyThread extends Thread {
+    String str;
+
+    public MyThread(String str) {
+        this.str = str;
+    }
+
+    /**
+     * Thread의 run() 메서드를 오버라이딩
+     *
+     * run()
+     * - main() 메서드와 비슷하지만, 다른 흐름의 main() 메서드이다.
+     * - 쓰레드 안에서 하고 싶은 일을 구현하면 된다.
+     */
+    @Override
+    public void run() {
+
+        // 생성자에서 받아들인 문자열을 10번 출력
+        for (int i = 0; i < 10; i++) {
+            System.out.println(str);
+
+            // sleep()이 발생시키는 exception을 처리
+            try {
+
+                // Thread.sleep(): 밀리초 쉰다 (1초 = 1000)
+                // Math.random(): 0.0 ~ 1.0 사이의 값 반환
+                Thread.sleep((int) (Math.random() * 1000));
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/thread/extendsthread/ThreadExam.java
@@ -0,0 +1,38 @@
+package thread.extendsthread;
+
+// MyThread를 사용하는 클래스
+public class ThreadExam {
+
+    /**
+     * main 쓰레드 (또는 main 흐름)
+     * - main 쓰레드가 종료되어도, 프로그램이 종료되는 것은 아니다.
+     * - 모든 쓰레드가 종료되어야만, 프로그램이 종료된다.
+     */
+   public static void main(String[] args) {
+
+        // 생성자에서 문자열을 받는다.
+        MyThread t1 = new MyThread("*");
+        MyThread t2 = new MyThread("-");
+
+        /**
+         * Thread를 동작시킬 때, run()이 아닌 start()를 호출해야 한다.
+         *
+         * start()
+         * - Thread가 실행할 준비를 하게 해주는 메서드
+         * - Thread가 실행할 준비가 다 되면, run()을 호출해준다.
+         *
+         * t1.start()를 실행할 때, 프로그램의 흐름이 2개가 된다.
+         * - 1) 원래 main()을 수행하던 흐름
+         * - 2) 쓰레드가 하나 더 발생돼서, 그 run()이 실행되는 흐름
+         *
+         * t2.start()를 실행할 때, 프로그램의 흐름이 3개가 된다.
+         * - 1) 원래 main()을 수행하던 흐름
+         * - 2) 쓰레드 1의 run()이 실행되는 흐름
+         * - 3) 쓰레드 2의 run()이 실행되는 흐름
+         */
+        t1.start();
+        t2.start();
+
+        System.out.println("main end !!!");
+    }
+}
